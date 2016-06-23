2df92505ae1d292dcc9d261d3cd5d480ab829f8a
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-06-23 15:14:39 -0400

    Rremove sample script, create commentBox

---
+++ public/scripts/commentBox.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const CommentBox = React.createClass({
+	render: () => {
+		return (
+			<div classname="commentBox">commentBox</div>
+		);
+	}
+});
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
